3465c3899a7dfb75ef5174c6cbfa2183122377f3
ilhamm017 <ilhammuliawan26@gmail.com>
2025-04-12 23:48:24 +0700

    feat: integrate Gemini AI for enhanced messaging responses
    - Added dotenv for environment variable management.
    - Updated app.js to include new message handling logic and improved client configuration.
    - Implemented Gemini AI services in geminiAi.js for generating responses.
    - Enhanced message queue management for personal messages.
    - Updated package.json and package-lock.json to include new dependencies: @google/genai, @google/generative-ai, dotenv, and nodemon.
    - Created .env file for storing sensitive API keys.
    - Updated .gitignore to exclude new authentication and cache files.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+require('dotenv').config()
 const {Client, LocalAuth} = require('whatsapp-web.js')
 const qrcode = require('qrcode-terminal')
+const {getGeminiResponse, geminiResponseAi} = require('./services/geminiAi')
 
 const client = new Client({
-    authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox'],
-        headless: false
-    }
-})
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    },
+    authStrategy: new LocalAuth()
+});
 
 client.initialize()
 
@@ -29,5 +31,124 @@ client.on('ready', () => {
     console.log('Client is ready!')
 })
 
+/**
+ * Map untuk menyimpan antrian pesan yang diterima.
+ */
+let messageQueue = new Map()
+ /**
+ * Berisi data dari pesan yang diterima.
+ */
+ let chatData = null
+ /**
+ * Sumber pesan (Pengirim) yang akan digunakan acuan untuk memproses data pengguna.
+ */
+ let sourceMessage = null
 
+// client.on('message', async (message) => {
+//     try {
+//         chatData = await message.getChat() 
+//         sourceMessage = message.from
 
+//         if(!chatData.isGroup) { // Jika pesan bukan dari grup
+//             if (!messageQueue.has(sourceMessage)) { // Jika antrian untuk pengirim ini belum ada, buat baru
+//                 messageQueue.set(sourceMessage, []);
+//             }
+//             if(message.body.startsWith('!')){ // Jika pesan diawali dengan '!'
+//                 messageQueue.get(sourceMessage).push(async () => {
+//                     try {
+//                         const response = await getGeminiResponse(message.body);
+//                         await client.sendMessage(sourceMessage, response);
+//                     } catch (err) {
+//                         console.error('Error processing message:', err);
+//                     }
+//                 });
+//             }
+//             const messages = await chatData.fetchMessages({
+//                 limit: 10
+//             });
+//             const chatHistory = messages.reverse().map(msg => ({
+//                 role: msg.fromMe ? 'model' : 'user',
+//                 parts: [{
+//                     text: msg.body
+//                 }]
+//             }));
+
+//             messageQueue.get(sourceMessage).push({
+//                 id: message.id._serialized,
+//                 from: message.from,
+//                 body: message.body,
+//                 replied: false,
+//                 timestamp: new Date().getTime()
+//             });
+             
+//             if (messageQueue.get(sourceMessage).length >= 1) {
+//                 while (messageQueue.get(sourceMessage).length > 0) {
+//                     const task = messageQueue.get(sourceMessage).shift();
+//                     await task();
+//                 }
+//                 messageQueue.delete(sourceMessage);
+//             }
+//         } else {
+//             console.log('Message is from a group, ignoring...');
+//         }
+//     } catch (error) {
+//         console.log('Error:', error);
+//     }   
+// });
+
+client.on('message_create', async (message) => {
+    console.log('message triggered')
+    try {
+        chatData = await message.getChat()
+        // console.log(chatData)
+        sourceMessage = message.from
+        const myMessage  = message.fromMe
+        // if (!pesanku) {
+        //     client.sendMessage(sourceMessage,'awkwkwkwk')
+        // }
+        if (!chatData.isGroup) {
+            console.log(`pesan personal: id ${message.id.id}`)
+            if (!messageQueue.has(sourceMessage)){
+                messageQueue.set(sourceMessage, [])
+            }
+            //=============================================================
+            if (message.hasQuotedMsg) {
+                const quoteData = await message.getQuotedMessage()
+                // const quoteId = quoteData.lastMessage._data.quotedStanzaID
+                // console.log(quoteData)
+                if (myMessage) {
+                    const sourceQuote = await client.getChatById(quoteData.from)
+                    if (messageQueue.get(quoteData.from).length >=1) {
+                        const arrayData = await messageQueue.get(quoteData.from).filter(item => !item.replied)
+                        console.log(arrayData)
+                        console.log(quoteData.from)
+                        if (arrayData) {
+                            const quotedMessageID = sourceQuote.lastMessage._data.quotedStanzaID
+                            console.log(quotedMessageID)
+                            const unRepliedMessage = await messageQueue.get(quoteData.from).filter(item => item.id != quotedMessageID )
+                            messageQueue.set(quoteData.from, unRepliedMessage)
+                            // Lanjut di sini (Perbaiki penghapusan data yang sudah di reply)
+
+                        }
+                    }
+                    console.log(`pesan quoted dari Message: id ${sourceQuote.lastMessage._data.quotedStanzaID}`)
+                }
+            }
+            //=============================================================
+            if (!myMessage) {
+                messageQueue.get(sourceMessage).push({
+                    id: chatData.lastMessage.id.id,
+                    from: message.from,
+                    body: message.body,
+                    replied: false,
+                    timestamp: message.timestamp
+                })
+                console.log(messageQueue.get(sourceMessage))
+            }
+        } else {
+            console.log(`pesan dari grub`)
+        }
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+    }
+})
---
+++ services/geminiAi.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const { GoogleGenerativeAI } = require('@google/generative-ai')
+const { GoogleGenAI } = require('@google/genai')
+const generativeAI = new GoogleGenerativeAI(process.env.Gemini_API_KEY)
+const genAI = new GoogleGenAI({
+    apiKey: process.env.Gemini_API_KEY
+})
+
+async function getGeminiResponse(prompt) {
+    const model = generativeAI.getGenerativeModel({
+        model: "gemini-2.0-flash"
+    })
+    const result = await model.generateContent(prompt)
+    const text = result.response.text()
+    return text
+}
+
+async function geminiResponseAi(chatHistory, prompt) {
+    const chat = genAI.chats.create({
+        model: "gemini-2.0-flash",
+        config: {
+            temperature: 2,
+            maxOutputTokens: 200,
+            systemInstruction: "Jawab pertanyaan dengan bahasa kasar, tapi kamu berperan seperti pelayan sexy yang rada menggoda"
+        },
+        history: chatHistory,
+    })
+    const result = await chat.sendMessage({
+        message: prompt
+    })
+    const text = result.text
+    return text
+}
+
+module.exports = {
+    getGeminiResponse,
+    geminiResponseAi
+}
